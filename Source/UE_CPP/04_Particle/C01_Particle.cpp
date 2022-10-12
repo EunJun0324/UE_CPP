961d999f628c34c958c334fec18b00c3513ff50f
EunJun0324 <92726410+EunJun0324@users.noreply.github.com>
2022-10-12 15:48:29 +0900

    1012

--- Source/UE_CPP/04_Particle/C01_Particle.cpp
+++ Source/UE_CPP/04_Particle/C01_Particle.cpp
@@ -28,6 +28,49 @@ void AC01_Particle::ActorBeginOverlap(AActor* OverlappedActor, AActor* OtherActo
 {
 	Particle->ResetParticles(); // Particle 을 리셋합니다.
 	Particle->SetActive(true); // Particle 을 활성화합니다.
+
+	FVector location = GetActorLocation();
+
+	TArray<TEnumAsByte<EObjectTypeQuery>> types;
+	types.Add(EObjectTypeQuery::ObjectTypeQuery4); // 충돌할 유형을 추가합니다.
+
+	TArray<AActor*> ignores; // 충돌 검사를 제외할 액터를 저장할 배열
+
+	TArray<FHitResult> hitResult; // 충돌 검사가 이루어진 액터를 저장할 배열
+
+	// SphereTraceMultiForObjects : 원형 충돌체를 일시적으로 생성합니다.
+	//								충돌한 물체가 있다면 true 없다면 false 를 반환합니다.
+	bool b = UKismetSystemLibrary::SphereTraceMultiForObjects(
+			 GetWorld(),	// SphereTrace 를 실행할 월드
+			 location,   // 시작지점
+			 location,   // 끝지점
+			 300,        // 반지름
+			 types,      // 충돌 검사 유형
+			 false,
+			 ignores,    // 충돌 검사 제외 목록
+			 DrawDebugType, // SphereTrace 그리기 설정
+			 hitResult,     // 충돌된 액터 목록
+			 true);		   // 자기 자신 충돌 제외 여부 
+
+	if (b) // 충돌한 물체가 존재한다면
+	{
+		// 충돌된 물체에 대하여 모두 반복합니다.
+		for (const FHitResult& hit : hitResult)
+		{
+			// 충돌된 액터의 RootComponent 를 가져옵니다.
+			UStaticMeshComponent* mesh = Cast<UStaticMeshComponent>(hit.GetActor()->GetRootComponent());
+
+			// RootComponent 가 UStaticMeshComponent 이며 해당 객체가 물리 객체라면
+			// IsSimulatingPhysics : 물리객체라면 ture 아니라면 false 를 반환합니다.
+			if (mesh && mesh->IsSimulatingPhysics())
+				mesh->AddRadialImpulse(
+					location,	// 폭발할 위치
+					1000,		// 폭발할 범위
+					500000.0f,  // 폭발할 힘의 크기
+					ERadialImpulseFalloff::RIF_MAX); // 폭발 기원으로부터의 강도 세기
+			// AddRadialImpulse : 충격을 가하는 기능
+		}
+	}
 }
 
 
