1a3f042e8761fa5dcde951737293ab5b159c6a86
EunJun0324 <92726410+EunJun0324@users.noreply.github.com>
2022-10-18 15:48:33 +0900

    1018

--- Source/UE_CPP/07_TPS/C_Bullet.h
+++ Source/UE_CPP/07_TPS/C_Bullet.h
@@ -9,9 +9,31 @@ class UE_CPP_API AC_Bullet : public AActor
 {
 	GENERATED_BODY()
 	
+private :
+	UPROPERTY(VisibleDefaultsOnly)
+		class UCapsuleComponent* Capsule; // 총알의 충돌체
+
+	UPROPERTY(VisibleDefaultsOnly)
+		class UStaticMeshComponent* Mesh;
+
+	UPROPERTY(EditDefaultsOnly)
+		class UProjectileMovementComponent* Projectile;
+	// 투사체의 물리적 이동을 담당하는 기능입니다.
+
 public:	
 	AC_Bullet();
 
 protected:
 	virtual void BeginPlay() override;
+
+private :
+	UFUNCTION()
+		void OnHit(UPrimitiveComponent* HitComponent,
+			AActor* OtherActor,
+			UPrimitiveComponent* OtherComp,
+			FVector NormalImpuluse,
+			const FHitResult& Hit);
+
+public :
+	void Shoot(const FVector& InDirection);
 };
