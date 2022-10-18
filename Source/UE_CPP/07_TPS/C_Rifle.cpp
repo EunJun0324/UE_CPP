87585d429c52848603f9ac66e6bbc36c37d06f59
EunJun0324 <92726410+EunJun0324@users.noreply.github.com>
2022-10-18 15:25:38 +0900

    1018

--- Source/UE_CPP/07_TPS/C_Rifle.cpp
+++ Source/UE_CPP/07_TPS/C_Rifle.cpp
@@ -29,6 +29,40 @@ AC_Rifle::AC_Rifle()
 {
 	PrimaryActorTick.bCanEverTick = true;
 
+	Mesh = CreateDefaultSubobject<USkeletalMeshComponent>(L"Mesh");
+
+	ConstructorHelpers::FObjectFinder<USkeletalMesh> mesh(L"SkeletalMesh'/Game/Weapons/Meshes/SK_AR4.SK_AR4'");
+	if (mesh.Succeeded()) Mesh->SetSkeletalMesh(mesh.Object);
+
+	ConstructorHelpers::FObjectFinder<UAnimMontage> grabMontage(L"AnimMontage'/Game/Character/Animations/Rifle/Rifle_Grab_Montage.Rifle_Grab_Montage'");
+	if (grabMontage.Succeeded()) GrapMontage = grabMontage.Object;
+
+	ConstructorHelpers::FObjectFinder<UAnimMontage> unGrabMontage(L"AnimMontage'/Game/Character/Animations/Rifle/Rifle_UnGrab_Montage.Rifle_UnGrab_Montage'");
+	if (unGrabMontage.Succeeded()) UnGrapMontage = unGrabMontage.Object;
+
+	ConstructorHelpers::FObjectFinder<UCurveFloat> curve(L"CurveFloat'/Game/Blueprints/07_TPS/Curve_Aim.Curve_Aim'");
+	if (curve.Succeeded()) Curve = curve.Object;
+
+	ConstructorHelpers::FObjectFinder<USoundCue> sound(L"SoundCue'/Game/Sounds/S_RifleShoot_Cue.S_RifleShoot_Cue'");
+	if (sound.Succeeded()) MuzzleSoundCue = sound.Object;
+
+	ConstructorHelpers::FObjectFinder<UParticleSystem> flash(L"ParticleSystem'/Game/Particles_Rifle/Particles/VFX_Muzzleflash.VFX_Muzzleflash'");
+	if (flash.Succeeded()) FlashParticle = flash.Object;
+
+	ConstructorHelpers::FObjectFinder<UParticleSystem> eject(L"ParticleSystem'/Game/Particles_Rifle/Particles/VFX_Eject_bullet.VFX_Eject_bullet'");
+	if (eject.Succeeded()) EjectParticle = eject.Object;
+
+	ConstructorHelpers::FObjectFinder<UParticleSystem> impact(L"ParticleSystem'/Game/Particles_Rifle/Particles/VFX_Impact_Default.VFX_Impact_Default'");
+	if (impact.Succeeded()) ImpactParticle = impact.Object;
+
+	ConstructorHelpers::FObjectFinder<UMaterialInstanceConstant> decal(L"MaterialInstanceConstant'/Game/Materials/M_Decal_Inst.M_Decal_Inst'");
+	if (decal.Succeeded()) ImpactDecal = decal.Object;
+
+	ConstructorHelpers::FClassFinder<UMatineeCameraShake> cameraShake(L"Blueprint'/Game/Blueprints/07_TPS/BP_CameraShake.BP_CameraShake_C'");
+	if (cameraShake.Succeeded()) CameraShake = cameraShake.Class;
+
+	ConstructorHelpers::FClassFinder<AC_Bullet> bullet(L"Blueprint'/Game/Blueprints/07_TPS/BP_Bullet.BP_Bullet_C'");
+	if (bullet.Succeeded()) BulletClass = bullet.Class;
 }
 
 void AC_Rifle::BeginPlay()
--- Source/UE_CPP/07_TPS/C_UserWidget.cpp
+++ Source/UE_CPP/07_TPS/C_UserWidget.cpp
@@ -1,5 +1,2 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "07_TPS/C_UserWidget.h"
 
